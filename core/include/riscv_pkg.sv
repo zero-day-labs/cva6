1efc141e81ab97f376face2f7339452ac67eeff0
Bruno Sa <bruno.vilaca.sa@gmail.com>
2021-06-13 00:09:47 +0100

    Modify PTW state machine to support G-Stage

--- core/include/riscv_pkg.sv
+++ core/include/riscv_pkg.sv
@@ -39,6 +39,7 @@ package riscv;
     // Warning: VLEN must be superior or equal to PLEN
     localparam VLEN       = (XLEN == 32) ? 32 : 64;    // virtual address length
     localparam PLEN       = (XLEN == 32) ? 34 : 56;    // physical address length
+    localparam GPLEN      = (XLEN == 32) ? 34 : 41;    // guest physical address length
 
     localparam IS_XLEN32  = (XLEN == 32) ? 1'b1 : 1'b0;
     localparam IS_XLEN64  = (XLEN == 32) ? 1'b0 : 1'b1;
@@ -46,8 +47,10 @@ package riscv;
     localparam ASIDW      = (XLEN == 32) ? 9 : 16;
     localparam VMIDW      = (XLEN == 32) ? 7 : 14;
     localparam PPNW       = (XLEN == 32) ? 22 : 44;
+    localparam GPPNW      = (XLEN == 32) ? 22 : 29;
     localparam vm_mode_t MODE_SV = (XLEN == 32) ? ModeSv32 : ModeSv39;
     localparam SV         = (MODE_SV == ModeSv32) ? 32 : 39;
+    localparam SVX        = (MODE_SV == ModeSv32) ? 34 : 41;
     localparam VPN2       = (VLEN-31 < 8) ? VLEN-31 : 8;
 
     typedef logic [XLEN-1:0] xlen_t;
@@ -304,7 +307,7 @@ package riscv;
     // memory management, pte for sv39
     typedef struct packed {
         logic [9:0]  reserved;
-        logic [44-1:0] ppn; // PPN length for 
+        logic [44-1:0] ppn; // PPN length for
         logic [1:0]  rsw;
         logic d;
         logic a;
@@ -318,7 +321,7 @@ package riscv;
 
     // memory management, pte for sv32
     typedef struct packed {
-        logic [22-1:0] ppn; // PPN length for 
+        logic [22-1:0] ppn; // PPN length for
         logic [1:0]  rsw;
         logic d;
         logic a;
