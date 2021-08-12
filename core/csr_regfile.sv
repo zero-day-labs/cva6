e8cb9bf9f17f5a00d6f3c28c5433622049fc1630
Bruno Sa <bruno.vilaca.sa@gmail.com>
2021-08-12 15:41:15 +0100

    fix comment csr name and field

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -1226,7 +1226,7 @@ module csr_regfile import ariane_pkg::*; #(
             mstatus_d.mpie = 1'b1;
             // set virtualization mode
             v_d            = mstatus_q.mpv;
-            //set hstatus spv to false
+            //set mstatus mpv to false
             mstatus_d.mpv  = 1'b0;
             if(mstatus_q.mpp != riscv::PRIV_LVL_M)
                 mstatus_d.mprv = 1'b0;
