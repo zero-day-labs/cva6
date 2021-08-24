b46be0cf95fe78e79712881e2b43473f5ca2a6f1
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2021-08-24 23:18:45 +0200

    Add FENCE_T_CEIL CSR
    Contains the maximal observed context switch latency.
    
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -96,6 +96,7 @@ module csr_regfile import ariane_pkg::*; #(
     input  logic                  wt_dcache_i,                // 0: WB DCache 1: WT DCache
     // fence.t
     output logic [31:0]           fence_t_pad_o,              // Padding time of fence.t relative to time interrupt
+    input  logic [31:0]           fence_t_ceil_i,
     // Performance Counter
     output logic  [4:0]           perf_addr_o,                // read/write address to performance counter module (up to 29 aux counters possible in riscv encoding.h)
     output logic[riscv::XLEN-1:0] perf_data_o,                // write data to performance counter module
@@ -178,6 +179,7 @@ module csr_regfile import ariane_pkg::*; #(
     riscv::xlen_t dcache_q,    dcache_d;
     riscv::xlen_t icache_q,    icache_d;
     riscv::xlen_t fence_t_pad_q, fence_t_pad_d;
+    riscv::xlen_t fence_t_ceil_q, fence_t_ceil_d;
 
     logic        wfi_d,       wfi_q;
 
@@ -407,6 +409,7 @@ module csr_regfile import ariane_pkg::*; #(
                 riscv::CSR_DCACHE:           csr_rdata = dcache_q;
                 riscv::CSR_ICACHE:           csr_rdata = icache_q;
                 riscv::CSR_FENCE_T_PAD:      csr_rdata = fence_t_pad_q;
+                riscv::CSR_FENCE_T_CEIL:     csr_rdata = fence_t_ceil_q;
                 // PMPs
                 riscv::CSR_PMPCFG0:          csr_rdata = pmpcfg_q[riscv::XLEN/8-1:0];
                 riscv::CSR_PMPCFG1:          if (riscv::XLEN == 32) csr_rdata = pmpcfg_q[7:4]; else read_access_exception = 1'b1;
@@ -517,6 +520,7 @@ module csr_regfile import ariane_pkg::*; #(
         dcache_d                = dcache_q;
         icache_d                = icache_q;
         fence_t_pad_d           = fence_t_pad_q;
+        fence_t_ceil_d          = {fence_t_ceil_q[63:32], (fence_t_ceil_i > fence_t_ceil_q[31:0]) ? fence_t_ceil_i : fence_t_ceil_q[31:0]};
 
         vsstatus_d              = vsstatus_q;
         vstvec_d                = vstvec_q;
@@ -914,6 +918,7 @@ module csr_regfile import ariane_pkg::*; #(
                 riscv::CSR_DCACHE:             dcache_d    = {{riscv::XLEN-1{1'b0}}, csr_wdata[0]}; // enable bit
                 riscv::CSR_ICACHE:             icache_d    = {{riscv::XLEN-1{1'b0}}, csr_wdata[0]}; // enable bit
                 riscv::CSR_FENCE_T_PAD:        fence_t_pad_d = {{riscv::XLEN-32{1'b0}}, csr_wdata[31:0]};
+                riscv::CSR_FENCE_T_CEIL:       fence_t_ceil_d = {{riscv::XLEN-31{1'b0}}, csr_wdata[32:0]};
                 // PMP locked logic
                 // 1. refuse to update any locked entry
                 // 2. also refuse to update the entry below a locked TOR entry
@@ -1584,6 +1589,7 @@ module csr_regfile import ariane_pkg::*; #(
             dcache_q               <= {{riscv::XLEN-1{1'b0}}, 1'b1};
             icache_q               <= {{riscv::XLEN-1{1'b0}}, 1'b1};
             fence_t_pad_q          <= {riscv::XLEN{1'b0}};
+            fence_t_ceil_q         <= {riscv::XLEN{1'b0}};
             // supervisor mode registers
             sepc_q                 <= {riscv::XLEN{1'b0}};
             scause_q               <= {riscv::XLEN{1'b0}};
@@ -1648,6 +1654,7 @@ module csr_regfile import ariane_pkg::*; #(
             dcache_q               <= dcache_d;
             icache_q               <= icache_d;
             fence_t_pad_q          <= fence_t_pad_d;
+            fence_t_ceil_q         <= fence_t_ceil_d;
             // supervisor mode registers
             sepc_q                 <= sepc_d;
             scause_q               <= scause_d;
