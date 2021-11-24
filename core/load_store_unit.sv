bf8fe9e1bc8dd918ad053c972de323d039b3f4c1
ninolomata <bruno.vilaca.sa@gmail.com>
2021-11-24 18:47:37 +0000

    fix rebase issues

--- core/load_store_unit.sv
+++ core/load_store_unit.sv
@@ -184,7 +184,9 @@ module load_store_unit import ariane_pkg::*; #(
             // icache address translation requests
             .icache_areq_i          ( icache_areq_i          ),
             .asid_to_be_flushed_i,
+            .vmid_to_be_flushed_i,
             .vaddr_to_be_flushed_i,
+            .gpaddr_to_be_flushed_i,
             .icache_areq_o          ( icache_areq_o          ),
             .pmpcfg_i,
             .pmpaddr_i,
@@ -432,15 +434,14 @@ module load_store_unit import ariane_pkg::*; #(
     // the misaligned exception is passed to the functional unit via the MMU, which in case
     // can augment the exception if other memory related exceptions like a page fault or access errors
     always_comb begin : data_misaligned_detection
-        automatic riscv::xlen_t tinst_addr_offset;
         misaligned_exception = {
+            {riscv::XLEN{1'b0}},
             {riscv::XLEN{1'b0}},
             {riscv::XLEN{1'b0}},
             {riscv::XLEN{1'b0}},
             1'b0,
             1'b0
         };
-        tinst_addr_offset = {riscv::XLEN{1'b0}};
         data_misaligned = 1'b0;
 
         if (lsu_ctrl.valid) begin
