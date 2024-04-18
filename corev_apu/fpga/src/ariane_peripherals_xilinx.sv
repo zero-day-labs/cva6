86048729be3bbe14a7d486192f0fd037827a4e7a
Manuel Rodríguez <manuel.cederog@gmail.com>
2024-04-18 10:06:22 +0100

    fix address width in IOMMU programming interface and update submodule
    Signed-off-by: Manuel Rodríguez <manuel.cederog@gmail.com>

--- corev_apu/fpga/src/ariane_peripherals_xilinx.sv
+++ corev_apu/fpga/src/ariane_peripherals_xilinx.sv
@@ -971,7 +971,7 @@ module ariane_peripherals #(
 
 		// Memory-mapped Register IF types
 		// name, addr_t, data_t, strb_t
-		`REG_BUS_TYPEDEF_ALL(iommu_reg, ariane_axi_soc::addr_t, ariane_axi_soc::data_t, ariane_axi_soc::strb_t)
+		`REG_BUS_TYPEDEF_ALL(iommu_reg, ariane_axi_soc::addr_t, logic [31:0], logic[3:0])
   
         riscv_iommu #(
             .IOTLB_ENTRIES	    ( 8	    					  ),
